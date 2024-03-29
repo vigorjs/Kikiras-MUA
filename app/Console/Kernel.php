8e030d98f52ce774aa94820496f5a88eafee07d1
unknown <111201912289@mhs.dinus.ac.id>
2024-03-30 04:38:12 +0700

    fix: fix artisan call for refresh tokens cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
+        $this->load(\Dymantic\InstagramFeed\Commands\RefreshTokens::class);
 
         require base_path('routes/console.php');
     }
--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dymantic\InstagramFeed\InstagramFeedServiceProvider;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
@@ -168,6 +169,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Dymantic\InstagramFeed\InstagramFeedServiceProvider::class,
+
     ])->toArray(),
 
     /*
